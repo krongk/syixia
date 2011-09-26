8dbd12228223529ae12551514d376c251a0796ea
krongk <kenrome@gmail.com>
2011-09-26 23:07:35 +0800

    add all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Syixia::Application.config.secret_token = '4bdee2bd3e2191e6bdd3d29be060609690faecf677f1cfd878775a99722fcdd3ef70fdfcd625c0c278d6356e4e2805ac3dc33ec827b9334cce19d1be0ff70ec6'
