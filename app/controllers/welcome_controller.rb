7552b6be61682efb848910378c07d1a5fb7e6711
unknown <Administrator@.(none)>
2011-09-29 20:21:13 +0800

    modified the search

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,10 @@ class WelcomeController < ApplicationController
     unless params[:q]
       render 'form', :layout => false
     end
+    ic = Iconv.new('UTF-8//IGNORE', 'gb2312')
     @coder = HTMLEntities.new
+    q = params[:q]
+    q = q.force_encoding("utf-8") unless q.nil?
     q = @coder.encode(params[:q])
     t = params[:t] || 'baidu_web'
     options = {:source => t.to_sym, :key_word => q}
