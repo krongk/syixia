504d364192a6226c7b27cd1113e09c7e1df22c43
krongk <kenrome@gmail.com>
2011-10-27 18:02:36 +0800

    degub fixed

--- config/initializers/i18n_yaml_generators.rb
+++ config/initializers/i18n_yaml_generators.rb
@@ -1,4 +1,4 @@
 Syixia::Application.config.i18n_yaml_generator.tap do |g|
   # Whether content in the generated locale files should be commented out (default: false)
-  g.commented = true 
+  g.commented = false 
 end
