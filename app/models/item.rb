2c9201930ad9a5cc97b49d9927decaad3998cda8
krongk <kenrome@gmail.com>
2011-11-08 22:54:44 +0800

    debug: engine

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,6 @@ class Item < ActiveRecord::Base
   belongs_to :cate
   has_one :item_value
 
-  scope :"百度网页", joins("inner join key_words k on key_word_id = k.id join engines e on k.engine_id = e.id WHERE e.id = 1")
-  scope :"奇虎问答", joins("inner join key_words k on key_word_id = k.id join engines e on k.engine_id = e.id WHERE e.id = 2")
+  scope :"百度网页", joins("inner join key_words k on key_word_id = k.id join engines e on k.engine_id = e.id WHERE e.name = 'baidu_web'")
+  scope :"奇虎问答", joins("inner join key_words k on key_word_id = k.id join engines e on k.engine_id = e.id WHERE e.name = 'qihoo_wenda'")
 end
--- app/models/key_word.rb
+++ app/models/key_word.rb
@@ -8,6 +8,6 @@ class KeyWord < ActiveRecord::Base
   	KeyWord.order("updated_at DESC").limit(count)
   end
 
-  scope :"百度网页", joins("inner join engines e on engine_id = e.id WHERE e.id = 1")
-  scope :"奇虎问答", joins("inner join engines e on engine_id = e.id WHERE e.id = 2")
+  scope :"百度网页", joins("inner join engines e on engine_id = e.id WHERE e.name = 'baidu_web'")
+  scope :"奇虎问答", joins("inner join engines e on engine_id = e.id WHERE e.name = 'qihoo_wenda'")
 end
