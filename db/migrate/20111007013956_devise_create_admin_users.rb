638f493a87f74b88f73522729a8433d6dd985efd
krongk <kenrome@gmail.com>
2011-11-10 16:51:05 +0800

    modify

--- db/migrate/20111007013956_devise_create_admin_users.rb
+++ db/migrate/20111007013956_devise_create_admin_users.rb
@@ -24,6 +24,7 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
 
     add_index :admin_users, :email,                :unique => true
     add_index :admin_users, :reset_password_token, :unique => true
+    #
     # add_index :admin_users, :confirmation_token,   :unique => true
     # add_index :admin_users, :unlock_token,         :unique => true
     # add_index :admin_users, :authentication_token, :unique => true
